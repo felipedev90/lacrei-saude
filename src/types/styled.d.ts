2085cb483b93d291a833d08a64e86706d1bb06d8
Felipe Augusto <augusto.felipedev@gmail.com>
2026-04-30 12:03:04 -0300

    fix(files): fix files prettier config

--- src/types/styled.d.ts
+++ src/types/styled.d.ts
@@ -1,7 +1,7 @@
-import { theme } from "@/styles/theme";
+import { theme } from '@/styles/theme'
 
-type ThemeType = typeof theme;
+type ThemeType = typeof theme
 
-declare module "styled-components" {
+declare module 'styled-components' {
   export interface DefaultTheme extends ThemeType {}
 }
