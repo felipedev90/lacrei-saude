2d7bddf51b86782050866e5f8f8572fcc1c6c11d
Felipe Augusto <augusto.felipedev@gmail.com>
2026-04-30 16:52:03 -0300

    feat(ecosystem): add ecosystem component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Header from '@/components/layout/Header'
 import Hero from '@/components/home/Hero'
+import Ecosystem from '@/components/home/Ecosystem'
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       <Header />
       <main>
         <Hero />
+        <Ecosystem />
       </main>
     </>
   )
