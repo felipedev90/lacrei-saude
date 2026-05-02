8a08ac328fb017c60bce9ed57babd229ace10cd6
Felipe Augusto <augusto.felipedev@gmail.com>
2026-05-02 14:00:55 -0300

    feat(cta): add cta section to quem somos and transform ctaBanner in a UI component

--- src/app/quem-somos/page.tsx
+++ src/app/quem-somos/page.tsx
@@ -2,6 +2,7 @@ import Hero from '@/components/quem-somos/Hero'
 import { ImageCardsGallery } from '@/components/quem-somos/ImageCards'
 import { NossaHistoria } from '@/components/quem-somos/NossaHistoria'
 import { NossasCrencas } from '@/components/quem-somos/NossasCrencas'
+import CtaBanner from '@/components/ui/CTA'
 
 export default function QuemSomosPage() {
   return (
@@ -10,6 +11,12 @@ export default function QuemSomosPage() {
       <ImageCardsGallery />
       <NossaHistoria />
       <NossasCrencas />
+      <CtaBanner
+        title="Faça parte do movimento"
+        subtitle="Junte-se a nós e ajude a construir um sistema de saúde mais justo e acolhedor."
+        primaryButton={{ label: 'Começar agora', href: '/' }}
+        secondaryButton={{ label: 'Quero ser voluntárie', href: '/quem-somos' }}
+      />
     </main>
   )
 }
